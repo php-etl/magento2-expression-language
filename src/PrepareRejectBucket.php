d9895953a67ecd9e1a6208abc3e6f86a2d0718bc
Jonathan <jonathan@kiboko.fr>
2023-10-18 17:33:58 +0200

    management of array with object

--- src/PrepareRejectBucket.php
+++ src/PrepareRejectBucket.php
@@ -21,39 +21,50 @@ class PrepareRejectBucket extends ExpressionFunction
     private function compile(string $input, int $depth = 2): string
     {
         return <<<PHP
-            (function(){
+            (function() use ($input){
                 \$serializer = function(\$input, int \$depth = 2) use (&\$serializer) {
-                    if (\$depth === 0 || !is_object(\$input)) {
+                    if (\$depth === 0 || !is_array(\$input) && !is_object(\$input)) {
                         return json_encode(\$input);
                     }
-                    \$object = new ReflectionObject(\$input);
                     \$output = [];
-                    foreach (\$object->getProperties() as \$property) {
-                        \$value = \$property->getValue(\$input);
-                        \$output[] = \$serializer(\$value, \$depth - 1);
+                    if (is_object(\$input)) {
+                        \$object = new ReflectionObject(\$input);
+                        foreach (\$object->getProperties() as \$property) {
+                            \$value = \$property->getValue(\$input);
+                            \$output[\$property->getName()] = \$serializer(\$value, \$depth - 1);
+                        }
+                    }
+                    if (is_array(\$input)) {
+                        foreach (\$input as \$key => \$value) {
+                            \$output[\$key] = \$serializer(\$value, \$depth - 1);
+                        }
                     }
                     return \$output;
                 };
-                return \$serializer($input, $depth)
+                return \$serializer(\$input, $depth);
             })()
             PHP;
     }
 
     private function evaluate(array $context, mixed $input, int $depth = 2): array
     {
-        $serializer = function($input, int $depth = 2) use (&$serializer) {
-            if ($depth === 0 || !is_object($input)) {
+        $serializer = function ($input, int $depth = 2) use(&$serializer) {
+            if ($depth === 0 || !is_array($input) && !is_object($input)) {
                 return json_encode($input);
             }
-
             $output = [];
-            $object = new ReflectionObject($input);
-
-            foreach ($object->getProperties() as $property) {
-                $value = $property->getValue($input);
-                $output[] = $serializer($value, $depth - 1);
+            if (is_object($input)) {
+                $object = new ReflectionObject($input);
+                foreach ($object->getProperties() as $property) {
+                    $value = $property->getValue($input);
+                    $output[$property->getName()] = $serializer($value, $depth - 1);
+                }
             }
-
+            if (is_array($input)) {
+                foreach ($input as $key => $value) {
+                    $output[$key] = $serializer($value, $depth - 1);
+                }
+            }
             return $output;
         };
 
