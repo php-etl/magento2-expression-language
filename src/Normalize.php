2b5aff650c2c263e05c01b0b9c0fdb0fb8f47fe5
Jonathan <jonathan@kiboko.fr>
2023-10-20 10:40:45 +0200

    update cs-fixer, add same rules we have on the distribution, and fix the project

--- src/Normalize.php
+++ src/Normalize.php
@@ -1,10 +1,9 @@
 <?php
 
+declare(strict_types=1);
+
 namespace Kiboko\Component\ExpressionLanguage\Magento;
 
-use Kiboko\Component\ExpressionLanguage\Magento\Serializable\ProductCatalogSerializableRejectData;
-use Kiboko\Component\ExpressionLanguage\Magento\Serializable\SerializableRejectDataInterface;
-use ReflectionObject;
 use Symfony\Component\ExpressionLanguage\ExpressionFunction;
 
 class Normalize extends ExpressionFunction
@@ -21,7 +20,7 @@ class Normalize extends ExpressionFunction
     private function compile(string $input, int $depth = 2): string
     {
         return <<<PHP
-            (function() use ($input){
+            (function() use ({$input}){
                 \$serializer = function(\$input, int \$depth = 2) use (&\$serializer) {
                     if (\$depth === 0 || !is_array(\$input) && !is_object(\$input)) {
                         return json_encode(\$input);
@@ -41,30 +40,31 @@ class Normalize extends ExpressionFunction
                     }
                     return \$output;
                 };
-                return \$serializer(\$input, $depth);
+                return \$serializer(\$input, {$depth});
             })()
             PHP;
     }
 
     private function evaluate(array $context, mixed $input, int $depth = 2): array
     {
-        $serializer = function ($input, int $depth = 2) use(&$serializer) {
-            if ($depth === 0 || !is_array($input) && !is_object($input)) {
+        $serializer = function ($input, int $depth = 2) use (&$serializer) {
+            if (0 === $depth || !\is_array($input) && !\is_object($input)) {
                 return json_encode($input);
             }
             $output = [];
-            if (is_object($input)) {
-                $object = new ReflectionObject($input);
+            if (\is_object($input)) {
+                $object = new \ReflectionObject($input);
                 foreach ($object->getProperties() as $property) {
                     $value = $property->getValue($input);
                     $output[$property->getName()] = $serializer($value, $depth - 1);
                 }
             }
-            if (is_array($input)) {
+            if (\is_array($input)) {
                 foreach ($input as $key => $value) {
                     $output[$key] = $serializer($value, $depth - 1);
                 }
             }
+
             return $output;
         };
 
